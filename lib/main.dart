0bf7bcd04d935074fe1cc7b1ef490477c32609d2
dashta <dashta>
2023-06-29 09:45:45 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Routing and Navigation"),
+          centerTitle: true,
+        ),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("Move to Page 2"),
+      ),
+    );
+  }
+}
+
